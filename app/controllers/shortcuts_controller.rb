4faab39422bbdcc5efcf393cd84f5ac9cf85fa5a
Michael Young <michaeljy@gmail.com>
2014-03-10 13:23:08 -0500

    form edit and create working on ONE step and ONE tag. need to add ability to add multiple steps and tags

--- app/controllers/shortcuts_controller.rb
+++ app/controllers/shortcuts_controller.rb
@@ -22,12 +22,32 @@ class ShortcutsController < ApplicationController
 
   # GET /shortcuts/1/edit
   def edit
+    puts "#{params[:id]}"
+    puts "---- PARAMS HERE #{params} ----"
+    @shortcut = Shortcut.find(params[:id])
+    @steps = @shortcut.steps
+    puts "---- STEPS HERE #{@steps} ----"
+    @shortcut_tag = ShortcutTag.where(shortcut_id: params[:id])
+    puts "---- SHORTCUT_TAG HERE #{@shortcut_tags} ----"
+    @tags = []
+    @shortcut_tag.each do |value|
+      @tags << Tag.find_by(id: value.tag_id)
+    end
   end
 
   # POST /shortcuts
   # POST /shortcuts.json
   def create
     @shortcut = Shortcut.new(shortcut_params)
+    @shortcut.save
+    @step = Step.new(step_params)
+    @step.shortcut_id = @shortcut.id
+    @step.save
+    @tag = Tag.new(tag_params)
+    @tag.save
+
+    @shortcut_tag = ShortcutTag.new(shortcut_id: @shortcut.id, tag_id: @tag.id)
+    @shortcut_tag.save
 
     respond_to do |format|
       if @shortcut.save
@@ -44,7 +64,9 @@ class ShortcutsController < ApplicationController
   # PATCH/PUT /shortcuts/1.json
   def update
     respond_to do |format|
-      if @shortcut.update(shortcut_params)
+      @step = Step.find_by(id: params[:step][:id])
+      @tag = Tag.find_by(id: params[:tag][:id])
+      if @shortcut.update(shortcut_params) && @step.update(step_params) && @tag.update(tag_params)
         format.html { redirect_to @shortcut, notice: 'Shortcut was successfully updated.' }
         format.json { head :no_content }
       else
@@ -73,5 +95,13 @@ class ShortcutsController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def shortcut_params
       params.require(:shortcut).permit(:title, :description)
+    end
+
+    def step_params
+      params.require(:step).permit(:shortcut_id, :step_number, :text, :image_url)
+    end
+
+    def tag_params
+      params.require(:tag).permit(:name)
     end
 end
