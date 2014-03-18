c6260fe443dd84fe4643269a291e4d051d985dfb
Michael Young <michaeljy@gmail.com>
2014-03-17 20:08:06 -0500

    search working by tags using a self.search method in shortcut model

--- app/models/shortcut.rb
+++ app/models/shortcut.rb
@@ -7,4 +7,13 @@ class Shortcut < ActiveRecord::Base
 
   accepts_nested_attributes_for :steps, :reject_if => :all_blank, :allow_destroy => true
   accepts_nested_attributes_for :tags, :reject_if => :all_blank, :allow_destroy => true
+
+  def self.search(search)
+    if search
+      shortcuts = Shortcut.includes(:tags).where("tags.name LIKE ?", "%#{search}%").references(:tags)
+    else
+      scoped
+    end
+  end
+
 end
